b684b9e7d96f5b4180685cbf91e7f0414fb4f477
Bruno Junqueira <brunootaviojunqueira@gmail.com>
2015-08-26 13:12:45 -0300

    View photo and video received and change launcher icon

--- app/src/main/java/com/brunojunqueira/ribbit/InboxFragment.java
+++ app/src/main/java/com/brunojunqueira/ribbit/InboxFragment.java
@@ -1,14 +1,31 @@
 package com.brunojunqueira.ribbit;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by brunojunqueira on 17/08/15.
  */
 public class InboxFragment extends android.support.v4.app.ListFragment {
+
+    protected List<ParseObject> mMessages;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,4 +35,69 @@ public class InboxFragment extends android.support.v4.app.ListFragment {
         return rootView;
     }
 
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(ParseConstants.CLASS_MESSAGES);
+        query.whereEqualTo(ParseConstants.KEY_RECIPIENTS_IDS, ParseUser.getCurrentUser().getObjectId());
+        query.addDescendingOrder(ParseConstants.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> messages, ParseException e) {
+                if ( e == null ) {
+                    mMessages = messages;
+
+                    String[] usernames = new String[mMessages.size()];
+                    int i = 0;
+                    for (ParseObject message : mMessages) {
+                        usernames[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
+                        i++;
+                    }
+                    if (getListView().getAdapter() == null) {
+                        MessageAdapter adapter = new MessageAdapter(
+                                getListView().getContext(),
+                                mMessages);
+                        setListAdapter(adapter);
+                    } else {
+                        ((MessageAdapter)getListView().getAdapter()).refill(mMessages);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ParseObject message = mMessages.get(position);
+        String messageType = message.getString(ParseConstants.KEY_FILE_TYPE);
+        ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
+        Uri fileUri = Uri.parse(file.getUrl());
+
+        if (messageType.equals(ParseConstants.TYPE_IMAGE)) {
+            Intent intent = new Intent(getActivity(), ViewImageActivity.class);
+            intent.setData(fileUri);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_VIEW, fileUri);
+            intent.setDataAndType(fileUri, "video/*");
+            startActivity(intent);
+        }
+
+        List<String> ids = message.getList(ParseConstants.KEY_RECIPIENTS_IDS);
+
+        if (ids.size() == 1) {
+            message.deleteInBackground();
+        } else {
+            ids.remove(ParseUser.getCurrentUser().getObjectId());
+            ArrayList<String> idsToRemove = new ArrayList<String>();
+            idsToRemove.add(ParseUser.getCurrentUser().getObjectId());
+
+            message.removeAll(ParseConstants.KEY_RECIPIENTS_IDS, idsToRemove);
+            message.saveInBackground();
+        }
+    }
 }
--- app/src/main/java/com/brunojunqueira/ribbit/MainActivity.java
+++ app/src/main/java/com/brunojunqueira/ribbit/MainActivity.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import android.app.AlertDialog;
@@ -285,14 +286,17 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
             case R.id.action_logout:
                 ParseUser.logOut();
                 navigateToLogin();
+                break;
             case R.id.action_edit_friends:
                 Intent intent = new Intent(this, EditFriendsActivity.class);
                 startActivity(intent);
+                break;
             case R.id.action_camera:
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setItems(R.array.camera_choices, mDialogListener);
                 AlertDialog dialog = builder.create();
                 dialog.show();
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
